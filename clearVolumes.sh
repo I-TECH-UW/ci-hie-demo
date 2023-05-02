c5590af92dd966b5e8f0bc0d6b619d8bb3e3f587
HerbertYiga <herbertyiga@gmail.com>
2023-05-02 23:05:30 +0300

    Clearing volumes

--- clearVolumes.sh
+++ clearVolumes.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 ###########################################
 #
-# Simple Shell script to clean/remove  opencr containers
+# Simple Shell script to clean/remove  opencr-fhir and  opencr containers
 #
 # The script will 
-#  - first stop the opencr containers 
-#  - remove opencr containers
+#  - first stop the opencr-fhir and opencr containers 
+#  - remove opencr-fhir containers
+#  - remove opencr
 #  - remove opencr images
-#  - remove opencr volumes
 #
 
-# stop opencr container
+# stop opencr and opencr-fhir container
 echo '####################################################'
-echo 'Stopping running opencr containers (if available)...'
+echo 'Stopping running opencr and opencr-fhir containers (if available)...'
 echo '####################################################'
-docker stop  opencr 
+docker stop  opencr opencr-fhir
+
+# remove  opencr-fhir stopped containers
+echo '####################################################'
+echo 'Removing opencr-fhir container ..'
+echo '####################################################'
+docker rm opencr-fhir
 
 # remove  opencr stopped containers
 echo '####################################################'
 echo 'Removing opencr container ..'
 echo '####################################################'
 docker rm opencr 
-
 
 # remove  opencr images
 echo '####################################################'
@@ -30,7 +35,7 @@ echo '####################################################'
 docker rmi intrahealth/opencr:latest
 
 # remove  opencr stray volumes if any
-echo '####################################################'
+echo '##################################################'
 echo 'Revoming  opencr container volumes (if any)'
 echo '####################################################'
-docker volume rm opencr 
+docker volume rm --force opencr
